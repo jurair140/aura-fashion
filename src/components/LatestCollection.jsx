00971cb9705c818fa968f0e42f77e4fcffcd09af
jurair <jurair140@gmail.com>
2024-11-01 10:26:50 +0530

    'fortheenth-commit'

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
 import ProductItem from './ProductItem'
+
 function LatestCollection() {
 
     const {products} = useContext(ShopContext)
